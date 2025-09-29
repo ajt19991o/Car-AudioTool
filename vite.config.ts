02d58bf53191b34115d60f12256f8f3ab85c31c0
ajt19991o <ajtoutant123@gmail.com>
2025-09-28 22:59:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { env } from 'node:process'
 
-const base = process.env.VITE_BASE_PATH ?? '/'
+const base = env.VITE_BASE_PATH ?? '/'
 
 // https://vite.dev/config/
 export default defineConfig({
